9abb69775ce8f81145387457e626e3bdba184ad8
Arshak Lazaryan <arshakr10@gmail.com>
2020-03-08 21:38:22 +0400

    arshak

--- scripts/main.js
+++ scripts/main.js
@@ -9,26 +9,21 @@ function save() {
   inputs[0]=document.getElementById('name').value;
      inputs[1]=document.getElementById('age').value;
      inputs[2]=document.getElementById('price').value;
-    
-    var theader = '<table border="1">\n';
-    var tbody = '';
-    for(var i = 0; i< inputs.length;i++){
+     var theader = '<table border="1">\n';
+     var tbody = '';
+     for(var i = 0; i< inputs.length;i++){
         tbody += '<tr>';
         tbody += '<td>';
-            tbody += inputs[i]  
+        tbody += inputs[i]  
         tbody += '</tr>\n';
     }
     var tfooter = '</table>';
     document.getElementById('wrapper').innerHTML = theader + tbody + tfooter;
-
-
     var name = document.getElementById('name').value;
     var age = document.getElementById('age').value;
     var price = document.getElementById('price').value;
-
     const person = new User(name, age, price);
     console.log('Save Pressed', person, name)
-
     fetch('http://localhost:8000/users/', {
       method: 'POST',
       headers: {
@@ -36,11 +31,11 @@ function save() {
       },
       body: JSON.stringify(person)
     })
-    .then(res => res.json())
-    .then(resp => {
+      .then(res => res.json())
+      .then(resp => {
         console.log('Response', resp);
     })
-    .catch(error => {
-      console.log('Error', error)
+     .catch(error => {
+     console.log('Error', error)
     });
 }
