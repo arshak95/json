54d53a6b40df120727cdfdba06058ade6c56208a
Arshak Lazaryan <arshakr10@gmail.com>
2020-03-26 18:30:37 +0400

    Arshak

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,6 @@ function User(name,age,price){
     this.age=age,
     this.price=price
 }
-
 function save() {
     const person = new User();
     person.name = document.getElementById('name').value;
@@ -25,37 +24,98 @@ function save() {
      console.log('Error', error)
     });
 }
+let editId = null;
+const renderUserRow = (users) => {
+  users.map((user, index) => {
+    const { name, age, price} = user;
 
-const table = document.getElementById('table');
-      const renderUserRow = (users) => {
-        console.log(users);
-        users.map((user) => {
-          const { name, age, price} = user;
-          const rowElement = document.createElement('tr');
-          const userName = document.createElement('td');
-          const userAge = document.createElement('td');
-          const userPrice = document.createElement('td');
-          const userEdit = document.createElement('button');
-          userEdit.innerHTML="EDIT";
-          userEdit.addEventListener("click", function(){
-            const answer = window.confirm("You want to change data?")
-               if (answer) {
-                
-               }
-                  else {
-                 }
-          });
-             
-          userName.innerText = name;
-          userAge.innerText = age;
-          userPrice.innerText = price;
-          rowElement.appendChild(userName);
-          rowElement.appendChild(userAge);
-          rowElement.appendChild(userPrice);
-          rowElement.appendChild(userEdit);
-          table.appendChild(rowElement);
-        })
-      };
+    let rowElement = document.getElementById(`user-${index}`); 
+    if (!rowElement) {
+      rowElement = document.createElement('tr');
+      rowElement.id = `user-${index}`;
+    }
+
+    let userName = document.getElementById(`userName-${index}`);
+    if (!userName) {
+      userName = document.createElement('td');
+      userName.id = `userName-${index}`;
+    }
+
+    let userAge = document.getElementById(`userAge-${index}`);
+    if (!userAge) {
+      userAge = document.createElement('td');
+      userAge.id = `userAge-${index}`;
+    }
+
+    let userPrice = document.getElementById(`userPrice-${index}`);
+    if (!userPrice) {
+      userPrice = document.createElement('td');
+      userPrice.id = `userPrice-${index}`;
+    }
+let userSave = document.getElementById(`userSave-${index}`);
+if(!userSave){
+  userSave = document.createElement('button');
+  userSave.innerHTML = "SAVE";
+  userSave.id = `userSave-${index}`;
+  userSave.addEventListener("click", function(){
+    fetch('http://localhost:8000/users/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify()
+    })
+      .then(res => res.json())
+      .then(resp => {
+        console.log('Response', resp);
+    })
+     .catch(error => {
+     console.log('Error', error)
+    });
+  })
+}
+    let userEdit = document.getElementById(`userEdit-${index}`);
+    if (!userEdit) {
+      userEdit = document.createElement('button');
+      userEdit.innerHTML="EDIT";
+      userEdit.id = `userEdit-${index}`;
+      userEdit.addEventListener("click", function(){
+        const answer = window.confirm("You want to change data?")
+        if (answer) {
+          editId = index;
+          renderUserRow(users);
+        } else {
+          console.log('2')
+        }
+      });
+    }
+
+    if (index === editId) {
+      userName.parentNode.removeChild(userName);
+      userName = document.createElement('input')
+      userName.value = name;
+      
+      userAge.parentNode.removeChild(userAge);
+      userAge = document.createElement('input')
+      userAge.value = age;
+      
+      userPrice.parentNode.removeChild(userPrice)
+      userPrice = document.createElement('input')
+      userPrice.value = price;
+    }
+        
+    userName.innerText = name;
+    userAge.innerText = age;
+    userPrice.innerText = price;
+
+    rowElement.appendChild(userName);
+    rowElement.appendChild(userAge);
+    rowElement.appendChild(userPrice);
+    rowElement.appendChild(userEdit);
+    table.appendChild(rowElement);
+    rowElement.appendChild(userSave);
+  })
+};
       (function (){
         fetch('http://localhost:8000/users/')
           .then(resp => {
@@ -66,7 +126,7 @@ const table = document.getElementById('table');
             console.log(data);
           })
           .catch(error => console.error(error))
-      })();
+  })();
   
 //   fetch('http://localhost:8000/users/')
 // .then(response => response.json())
